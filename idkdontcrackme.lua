66893c473203e012dbfcbf4231f013ce1b878c71
Sanli Saeti <azukiairi777@gmail.com>
2025-06-14 03:34:10 +0700

    Update idkdontcrackme.lua

--- idkdontcrackme.lua
+++ idkdontcrackme.lua
@@ -1,2 +1,6 @@
 repeat task.wait() until game:IsLoaded()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/gukonsakonnakhon/FreeSrc/refs/heads/main/Protected_7312332916120313.lua"))()
+if getgenv().togglegui == nil then return end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gukonsakonnakhon/FreeSrc/refs/heads/main/fuckingtoggle.lua"))()
+if getgenv().togglegui == true then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/gukonsakonnakhon/FreeSrc/refs/heads/main/Protected_7312332916120313.lua"))()
+end
